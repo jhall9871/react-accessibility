87451c58e3baeac75db2db86b8fc21ec618b1b14
James Hall <jhall9871@gmail.com>
2021-02-08 13:29:27 -0600

    adds functions, updates readme

--- moz-todo-react/src/App.js
+++ moz-todo-react/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
 function App(props) {
-  const taskList = props.tasks.map((task) => (
+  const [tasks, setTasks] = useState(props.tasks);
+  const taskList = tasks.map((task) => (
     <Todo
       id={task.id}
       name={task.name}
@@ -14,7 +15,8 @@ function App(props) {
   ));
 
   function addTask(name) {
-    alert(name);
+    const newTask = { id: "id", name: name, completed: false };
+    setTasks([...tasks, newTask]);
   }
 
   return (
